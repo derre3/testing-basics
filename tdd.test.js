d18181e7040d7792cdb888fb62ab88c653d5c1f0
andre <123985988+derre3@users.noreply.github.com>
2023-12-07 16:18:01 -0300

    write tests

--- tdd.test.js
+++ tdd.test.js
@@ -1,9 +1,47 @@
 import * as tdd from './tdd';
 
-test('1 + 2 to equal 3', () => {
-  expect(tdd.sum(1, 2)).toBe(3);
+test('capitalize string', () => {
+  expect(tdd.capitalize('string')).toBe('String');
 });
 
-test('6 - 2 to equal 4', () => {
-  expect(tdd.sub(6, 2)).toBe(4);
+test('reverse string', () => {
+  expect(tdd.reverseString('string')).toBe('gnirts');
+});
+
+test('calculator add', () => {
+  expect(tdd.calculator().add(2, 1)).toBe(3);
+});
+test('calculator subtract', () => {
+  expect(tdd.calculator().sub(2, 1)).toBe(1);
+});
+test('calculator divide', () => {
+  expect(tdd.calculator().div(4, 2)).toBe(2);
+});
+test('calculator multiply', () => {
+  expect(tdd.calculator().mult(2, 3)).toBe(6);
+});
+
+test('caesar cypher abc -> def', () => {
+  expect(tdd.caesarCipher('abc', 3)).toBe('def');
+});
+test('caesar cypher xyz -> abc', () => {
+  expect(tdd.caesarCipher('xyz', 3)).toBe('abc');
+});
+test('caesar cypher AbC -> DeF', () => {
+  expect(tdd.caesarCipher('AbC', 3)).toBe('DeF');
+});
+test('caesar cypher XyZ -> AbC', () => {
+  expect(tdd.caesarCipher('XyZ', 3)).toBe('AbC');
+});
+test('caesar cypher FGh? -> IJk?', () => {
+  expect(tdd.caesarCipher('FGh?', 3)).toBe('IJk?');
+});
+
+test('analyze array', () => {
+  expect(tdd.analyzeArray([3, 5, 7, 9])).toEqual({
+    average: 6,
+    min: 3,
+    max: 9,
+    length: 4,
+  });
 });
